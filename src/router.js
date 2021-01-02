c0239f70b8d05093963ec1ca9343637c991e4957
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2021-01-02 23:53:35 +0300

    router re write

--- src/router.js
+++ src/router.js
@@ -3,12 +3,16 @@ import VueRouter from "vue-router";
 
 import Index from "./components/HelloWorld";
 import NotFound from "./components/NotFound";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/", component: Index },
-  { path: "/notfound", component: NotFound }
+  { path: "/notfound", component: NotFound },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register }
 ];
 
 export const router = new VueRouter({
