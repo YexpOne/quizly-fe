c7600d7dfa2bd9520a731400d1264ca19d5b2cad
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2021-01-02 22:12:30 +0300

    notfound page add

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <NotFound />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NotFound from './components/NotFound.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NotFound
   }
 }
 </script>
